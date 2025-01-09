91d03d81cd0bada4324e558d9737cf71d3763c2f
Aryan-Kumr <152487056+Aryan-Kumr@users.noreply.github.com>
2025-01-10 00:33:39 +0530

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,5 +1,43 @@
 # prg-5
+# small
+import pandas as pd
+from sklearn.datasets import load_iris
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsClassifier
 
+iris = load_iris()
+iris.feature_names
+iris.target_names
+df = pd.DataFrame(iris.data,columns=iris.feature_names)
+# df.head()
+
+df['target'] = iris.target
+# df.head(-5)
+
+df[df.target==1].head()
+df0 = df[df.target==0]
+df1 = df[df.target==1]
+df2 = df[df.target==2]
+df2.head()
+
+plt.xlabel('Sepal Length')
+plt.ylabel('Sepal Width')
+plt.scatter(df0['sepal length (cm)'], df0['sepal width (cm)'],color="green",marker='+')
+plt.scatter(df1['sepal length (cm)'], df1['sepal width (cm)'],color="blue",marker='.')
+
+X = df.drop(['target'], axis='columns')
+y = df.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=1)
+len(X_train)
+len(X_test)
+
+knn = KNeighborsClassifier(n_neighbors=10)
+knn.fit(X_train, y_train)
+knn.score(X_test, y_test)
+
+# ------------------------------------------------------------------------------------------------------------------------------------------------------------------
+# large
 from sklearn.datasets import load_iris
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
